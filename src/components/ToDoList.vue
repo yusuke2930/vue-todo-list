c41b7778fc46f17d9435524e4cf6c2cc0c4fa006
Yusuke Katori <yusuke2930@gmail.com>
2021-03-25 04:39:12 +0900

    v-model

--- src/components/ToDoList.vue
+++ src/components/ToDoList.vue
@@ -1,4 +1,6 @@
 <template>
+    <input v-model="inputValue">
+    入力した値: {{ inputValue }}
     <ul>
         <li v-for="todo in todoItems" v-bind:key="todo.id">
             {{ todo.text }}
@@ -9,6 +11,7 @@
 export default {
     data() {
         return {
+            inputValue: '',
             todoItems: [
                 { id: 1, text: 'Go out to sea' },
                 { id: 2, text: 'Invite the first member' },
