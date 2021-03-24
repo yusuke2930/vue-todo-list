87df6d4ea79658f5267109c53827b3dbdeea49bc
Yusuke Katori <yusuke2930@gmail.com>
2021-03-25 03:47:12 +0900

    v-bind

---
+++ src/components/ToDoList.vue
@@ -0,0 +1,16 @@
+<template>
+    <ul>
+        <li v-bind:title="todo">
+            {{ todo.slice(0, 10) + '...' }}
+        </li>
+    </ul>
+</template>
+<script>
+export default {
+    data() {
+        return {
+            todo: 'Go out to sea'
+        }
+    }
+}
+</script>
